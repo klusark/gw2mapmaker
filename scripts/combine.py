99c9b7fd5eb15ef168bb556b48a03e268432917b
Joel Teichroeb <joel@teichroeb.net>
2014-02-11 21:10:18 +0000

    Improve the combine script

--- scripts/combine.py
+++ scripts/combine.py
@@ -3,37 +3,44 @@
 from PIL import Image
 import os
 
-tolayer = 1
+#tolayer = 6
+def processImage(newimage, x, y, pos):
+	try:
+		im = Image.open(basepath + "/" + x + "/" + y + filetype)
+		newimage.paste(im, pos);
+	except:
+		None
 
-layermax = 2**tolayer
+for tolayer in (5,4,3,2,1):
 
-for x in range(0,layermax):
-	for y in range(0,layermax):
-		print(x, y);
-		x1 = x*2;
-		x2 = x1 + 1;
-		y1 = y*2;
-		y2 = y1 + 1;
-		x1 = str(x1);
-		x2 = str(x2);
-		y1 = str(y1);
-		y2 = str(y2);
-		basepath = "tiles/1/1/"+str(tolayer+1);
-		im1 = Image.open(basepath + "/" + x1 + "/" + y1 + ".jpg")
-		im2 = Image.open(basepath + "/" + x2 + "/" + y1 + ".jpg")
-		im3 = Image.open(basepath + "/" + x1 + "/" + y2 + ".jpg")
-		im4 = Image.open(basepath + "/" + x2 + "/" + y2 + ".jpg")
+	layermax = 2**tolayer
 
-		newimage = Image.new("RGB", (512,512));
-		newimage.paste(im1, (0,0));
-		newimage.paste(im2, (256,0));
-		newimage.paste(im3, (0,256));
-		newimage.paste(im4, (256,256));
-		newimage.thumbnail((256,256), Image.ANTIALIAS);
+	basepath = "tiles/1/1/"+str(tolayer+1);
+	filetype = ".png"
+
+
+	for x in range(0,layermax):
+		for y in range(0,layermax):
+			print(x, y);
+			x1 = x*2;
+			x2 = x1 + 1;
+			y1 = y*2;
+			y2 = y1 + 1;
+			x1 = str(x1);
+			x2 = str(x2);
+			y1 = str(y1);
+			y2 = str(y2);
+			newimage = Image.new("RGBA", (512,512));
+			processImage(newimage, x1, y1, (0,0));
+			processImage(newimage, x2, y1, (256,0));
+			processImage(newimage, x1, y2, (0,256));
+			processImage(newimage, x2, y2, (256,256));
+
+			newimage.thumbnail((256,256), Image.ANTIALIAS);
+
+			dir = "tiles/1/1/"+str(tolayer)+"/"+str(x);
+			if not os.path.exists(dir):
+				os.makedirs(dir);
+			out = dir + "/" + str(y) + filetype
+			newimage.save(out);
 			
-		dir = "tiles/1/1/"+str(tolayer)+"/"+str(x);
-		if not os.path.exists(dir):
-			os.makedirs(dir);
-		out = dir + "/" + str(y) + ".jpg"
-		newimage.save(out);
-		
